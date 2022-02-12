a1debae6bac2d5b03de66ef8ee5f83d0ff8605f7
Christy Jestin <cjj9000@gmail.com>
2022-02-11 20:10:38 -0500

    Part 4

--- HW1/T1_P4.py
+++ HW1/T1_P4.py
@@ -1,9 +1,3 @@
-#####################
-# CS 181, Spring 2022
-# Homework 1, Problem 4
-# Start Code
-##################
-
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,81 +8,70 @@ republican_counts = []
 sunspot_counts = []
 
 with open(csv_filename, 'r') as csv_fh:
-
-    # Parse as a CSV file.
-    reader = csv.reader(csv_fh)
-
-    # Skip the header line.
-    next(reader, None)
-
-    # Loop over the file.
+    reader = csv.DictReader(csv_fh)
     for row in reader:
+        years.append(float(row['Year']))
+        sunspot_counts.append(float(row['Sunspot_Count']))
+        republican_counts.append(float(row['Republican_Count']))
 
-        # Store the data.
-        years.append(float(row[0]))
-        sunspot_counts.append(float(row[1]))
-        republican_counts.append(float(row[2]))
-
-# Turn the data into numpy arrays.
 years  = np.array(years)
 republican_counts = np.array(republican_counts)
 sunspot_counts = np.array(sunspot_counts)
 last_year = 1985
 
-# Plot the data.
-plt.figure(1)
-plt.plot(years, republican_counts, 'o')
-plt.xlabel("Year")
-plt.ylabel("Number of Republicans in Congress")
-plt.figure(2)
-plt.plot(years, sunspot_counts, 'o')
-plt.xlabel("Year")
-plt.ylabel("Number of Sunspots")
-plt.figure(3)
-plt.plot(sunspot_counts[years<last_year], republican_counts[years<last_year], 'o')
-plt.xlabel("Number of Sunspots")
-plt.ylabel("Number of Republicans in Congress")
-plt.show()
-
-# Create the simplest basis, with just the time and an offset.
-X = np.vstack((np.ones(years.shape), years)).T
-
-# TODO: basis functions
 # Based on the letter input for part ('a','b','c','d'), output numpy arrays for the bases.
-# The shape of arrays you return should be: (a) 24x6, (b) 24x12, (c) 24x6, (c) 24x26
-# xx is the input of years (or any variable you want to turn into the appropriate basis).
-# is_years is a Boolean variable which indicates whether or not the input variable is
-# years; if so, is_years should be True, and if the input varible is sunspots, is_years
-# should be false
-def make_basis(xx,part='a',is_years=True):
-#DO NOT CHANGE LINES 65-69
-    if part == 'a' and is_years:
-        xx = (xx - np.array([1960]*len(xx)))/40
-        
-    if part == "a" and not is_years:
-        xx = xx/20
-        
-        
-    return None
+def make_basis(x, part = 'a', is_years = True):
+    assert(part in ['a', 'b', 'c', 'd'])
+    if part == 'a':
+        scaled = ((x - 1960) / 40) if is_years else (x / 20)
+        basis = [scaled ** i for i in range(1, 6)]
+    if part == 'b':
+        assert(is_years)
+        basis = [np.exp((x - i) ** 2 / -25) for i in range(1960, 2011, 5)]
+    if part == 'c':
+        basis = [np.cos(x / i) for i in range(1, 6)]
+    if part == 'd':
+        basis = [np.cos(x / i) for i in range(1, 26)]
 
-# Nothing fancy for outputs.
-Y = republican_counts
+    # add basis for bias and stack all basis vectors
+    return np.vstack((np.ones(x.shape), *basis)).T
 
 # Find the regression weights using the Moore-Penrose pseudoinverse.
-def find_weights(X,Y):
-    w = np.dot(np.linalg.pinv(np.dot(X.T, X)), np.dot(X.T, Y))
-    return w
+def find_weights(X, Y):
+    return np.dot(np.linalg.pinv(np.dot(X.T, X)), np.dot(X.T, Y))
 
-# Compute the regression line on a grid of inputs.
-# DO NOT CHANGE grid_years!!!!!
-grid_years = np.linspace(1960, 2005, 200)
-grid_X = np.vstack((np.ones(grid_years.shape), grid_years))
-grid_Yhat  = np.dot(grid_X.T, w)
+def compute_loss(Y, Yhat):
+    return np.sum((Y - Yhat) ** 2)
 
-# TODO: plot and report sum of squared error for each basis
+if __name__ == '__main__':
+    parts = ['a', 'b', 'c', 'd']
+    grid_years = np.linspace(1960, 2005, 200)
+    for part in parts:
+        years_basis = make_basis(years, part = part)
+        w = find_weights(years_basis, republican_counts)
+        loss = compute_loss(republican_counts, np.dot(years_basis, w))
+        print(f"Loss for Years (part {part}): {loss}")
+        grid_Yhat  = np.dot(make_basis(grid_years, part = part), w)
+        # Plot the data and the regression line.
+        plt.plot(years, republican_counts, 'o', grid_years, grid_Yhat, '-')
+        plt.xlabel("Year")
+        plt.ylabel("Number of Republicans in Congress")
+        plt.show()
 
-# Plot the data and the regression line.
-plt.plot(years, republican_counts, 'o', grid_years, grid_Yhat, '-')
-plt.xlabel("Year")
-plt.ylabel("Number of Republicans in Congress")
-plt.show()
+    sunspot_counts = sunspot_counts[years<last_year]
+    republican_counts = republican_counts[years<last_year]
+    grid_sunspots = np.linspace(0, 175, 800)
+    for part in parts:
+        if part == 'b':
+            continue
+        sunspots_basis = make_basis(sunspot_counts, part = part, is_years = False)
+        w = find_weights(sunspots_basis, republican_counts)
+        loss = compute_loss(republican_counts, np.dot(sunspots_basis, w))
+        print(f"Loss for Sunspots (part {part}): {loss}")
+        grid_Yhat  = np.dot(make_basis(grid_sunspots, part = part, is_years = False), w)
+        # Plot the data and the regression line.
+        plt.plot(sunspot_counts, republican_counts, 'o', grid_sunspots, grid_Yhat, '-')
+        plt.ylim([0, 200])
+        plt.xlabel("Number of Sunspots")
+        plt.ylabel("Number of Republicans in Congress")
+        plt.show()
