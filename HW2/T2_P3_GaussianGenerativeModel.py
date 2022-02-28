fc5ca9800248628da9d2ff388aab601c7e27d150
Christy Jestin <cjj9000@gmail.com>
2022-02-27 21:27:29 -0500

    Implement P3 models

--- HW2/T2_P3_GaussianGenerativeModel.py
+++ HW2/T2_P3_GaussianGenerativeModel.py
@@ -1,36 +1,47 @@
 import numpy as np
-from scipy.stats import multivariate_normal as mvn  # you may find this useful
+from scipy.stats import multivariate_normal as mvn
 
-
-# Please implement the fit(), predict(), and negative_log_likelihood() methods
-# of this class. You can add additional private methods by beginning them with
-# two underscores. It may look like the __dummyPrivateMethod below. You can feel
-# free to change any of the class attributes, as long as you do not change any
-# of the given function headers (they must take and return the same arguments).
+# ----------------------------NB------------------------------
+# ALL CODE BELOW ASSUMES THAT THE CLASSES ARE 0, 1, 2,..., K-1
+# --------------------------IMPORTANT-------------------------
 
 class GaussianGenerativeModel:
     def __init__(self, is_shared_covariance=False):
         self.is_shared_covariance = is_shared_covariance
+        self.pi = None
+        self.mu = None
+        self.Sigma = None
+        self.labels = None
+        self.gaussians = None
 
-    # Just to show how to make 'private' methods
-    def __dummyPrivateMethod(self, input):
-        return None
-
-    # TODO: Implement this method!
     def fit(self, X, y):
-        return
+        self.labels, counts = np.unique(y, return_counts=True)
+        self.pi = counts / len(y)
+        self.mu = np.array([np.mean(X[np.where(y == i)], axis=0) for i in self.labels])
+        if self.is_shared_covariance:
+            mus = np.array([self.mu[i] for i in y])
+            # diff is m x n where n is length of a single mu and m is number of data points
+            # and we want to end up with n x n; note that m = len(y)
+            diff = X - mus
+            self.Sigma = diff.T @ diff / len(y)
+            self.gaussians = [mvn(mean=self.mu[i], cov=self.Sigma) for i in self.labels]
+        else:
+            def sigma(label):
+                X_in_class = X[np.where(y == label)]
+                # diff is m x n where n is length of a single mu and m is number of data points in class i
+                # and we want to end up with n x n; note that m = len(X_in_class)
+                diff = X_in_class - self.mu[label:label+1]
+                return diff.T @ diff / len(X_in_class)
+            self.Sigma = [sigma(i) for i in self.labels]
+            self.gaussians = [mvn(mean=self.mu[i], cov=self.Sigma[i]) for i in self.labels]
 
-    # TODO: Implement this method!
     def predict(self, X_pred):
-        # The code in this method should be removed and replaced! We included it
-        # just so that the distribution code is runnable and produces a
-        # (currently meaningless) visualization.
-        preds = []
-        for x in X_pred:
-            z = np.sin(x ** 2).sum()
-            preds.append(1 + np.sign(z) * (np.abs(z) > 0.3))
-        return np.array(preds)
+        # p(y|x) is proportional to p(x|y) * p(y)
+        def pred(x):
+            return np.argmax([self.gaussians[i].pdf(x) * self.pi[i] for i in self.labels])
+        return np.array([pred(x) for x in X_pred])
 
-    # TODO: Implement this method!
     def negative_log_likelihood(self, X, y):
-        pass
+        def log_likelihood(x, label):
+            return self.gaussians[label].logpdf(x) * np.log(self.pi[label])
+        return -np.sum([log_likelihood(X[i], y[i]) for i in range(len(y))])
--- HW2/T2_P3_LogisticRegression.py
+++ HW2/T2_P3_LogisticRegression.py
@@ -1,37 +1,60 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+# ----------------------------NB------------------------------
+# ALL CODE BELOW ASSUMES THAT THE CLASSES ARE 0, 1, 2,..., K-1
+# --------------------------IMPORTANT-------------------------
 
-
-# Please implement the fit(), predict(), and visualize_loss() methods of this
-# class. You can add additional private methods by beginning them with two
-# underscores. It may look like the __dummyPrivateMethod below. You can feel
-# free to change any of the class attributes, as long as you do not change any
-# of the given function headers (they must take and return the same arguments).
+ITERATION_BLOCK = 100
+MAX_ITERATIONS = 200000
 
 class LogisticRegression:
     def __init__(self, eta, lam):
         self.eta = eta
         self.lam = lam
+        self.W = None
+        self.losses = None
 
-    # Just to show how to make 'private' methods
-    def __dummyPrivateMethod(self, input):
-        return None
+    def __add_ones(self, X):
+        return np.hstack((np.ones((len(X), 1)), X))
 
-    # TODO: Implement this method!
+    def __y_hat(self, X):
+        y_hat = np.exp(np.dot(X, self.W))
+        return y_hat / np.linalg.norm(y_hat, ord=1, axis=1, keepdims=True)
+
+    def __step(self, X, y):
+        y_hat = self.__y_hat(X)
+        def column_gradient(k):
+            return np.sum(X * y_hat[:, k:k+1], axis=0) - np.sum(X[np.where(y == k)], axis=0)
+        gradient = np.hstack([np.expand_dims(column_gradient(k), axis=1) for k in self.labels])
+        regularizer = 2 * self.lam * self.W
+        self.W -= (gradient + regularizer) * self.eta
+
+    def __compute_loss(self, X, y):
+        return -np.sum([np.log(self.__y_hat(X))[i, v] for i, v in enumerate(y)])
+
     def fit(self, X, y):
-        return
+        self.labels = np.unique(y)
+        self.W = np.random.rand(X.shape[1] + 1, len(self.labels))
+        self.losses = []
+        for i in range(MAX_ITERATIONS):
+            self.__step(self.__add_ones(X), y)
+            if i % ITERATION_BLOCK == 0:
+                self.losses.append(self.__compute_loss(self.__add_ones(X), y))
 
-    # TODO: Implement this method!
+
     def predict(self, X_pred):
-        # The code in this method should be removed and replaced! We included it
-        # just so that the distribution code is runnable and produces a
-        # (currently meaningless) visualization.
-        preds = []
-        for x in X_pred:
-            z = np.cos(x ** 2).sum()
-            preds.append(1 + np.sign(z) * (np.abs(z) > 0.3))
-        return np.array(preds)
+        return np.argmax(np.dot(self.__add_ones(X_pred), self.W), axis=1)
 
-    # TODO: Implement this method!
     def visualize_loss(self, output_file, show_charts=False):
-        pass
+        x = np.arange(0, len(self.losses) * ITERATION_BLOCK, ITERATION_BLOCK) + 1
+        y = np.array(self.losses)
+        plt.figure()
+        plt.title('Loss over Iterations')
+        plt.xlabel('Number of Iterations')
+        plt.ylabel('Negative Log-Likelihood Loss')
+        plt.plot(x, y)
+        # Saving the image to a file, and showing it as well
+        plt.savefig(output_file + '.png')
+        if show_charts:
+            plt.show()
